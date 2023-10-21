815a6055f15cbae387ea4e4f84fe29c4f7718d55
L0P4 <95104391+L0PA@users.noreply.github.com>
2023-10-21 14:18:03 +0200

    Update install_haxl0p4.sh

--- install_haxl0p4.sh
+++ install_haxl0p4.sh
@@ -18,13 +18,22 @@ if ! command -v proxychains >/dev/null; then
     sudo apt-get install proxychains -y
 fi
 
-# Aggiorna la cache dei repository
-sudo apt-get update
-
 # Verifica ed installazione di arp-scan
 if ! command -v arp-scan >/dev/null; then
     echo "Installing arp-scan..."
     sudo apt-get install arp-scan -y
+fi
+
+# Verifica ed installazione di msfconsole e msfvenom
+if ! command -v msfconsole >/dev/null || ! command -v msfvenom >/dev/null; then
+    # Aggiorna la cache dei repository
+    sudo apt-get update
+
+    # Installa Metasploit Framework
+    sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-keys ED444FF07D8D0BF6
+    echo "deb http://apt.metasploit.com/ lucid main" | sudo tee /etc/apt/sources.list.d/metasploit-framework.list
+    sudo apt-get update
+    sudo apt-get install metasploit-framework -y
 fi
 
 # Esegui il programma Python
